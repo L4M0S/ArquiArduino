88e96daab5ef73b2adbbd2288f1791f67afb9364
L4M0S <13170563+L4M0S@users.noreply.github.com>
2018-12-21 21:40:45 -0600

    Final

---
+++ P8/P8.ino
@@ -0,0 +1,74 @@
+int doo=261;
+int re=293;
+int mi=329;
+int fa=349;
+int sol=392;
+int la=440;
+int si=493;
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  tone(2,mi);
+  delay(500);
+  noTone(2);
+  
+  tone(2,mi);
+  delay(500);
+  noTone(2);
+  
+  tone(2,fa);
+  delay(500);
+  noTone(2);
+  
+  tone(2,sol);
+  delay(500);
+  noTone(2);
+  
+  //delay(500);
+  
+  tone(2,sol);
+  delay(500);
+  noTone(2);
+  
+  tone(2,fa);
+  delay(500);
+  noTone(2);
+  
+  tone(2,mi);
+  delay(500);
+  noTone(2);
+
+  tone(2,re);
+  delay(500);
+  noTone(2);
+
+  tone(2,doo);
+  delay(500);
+  noTone(2);
+  tone(2,doo);
+  delay(500);
+  noTone(2);
+  tone(2,re);
+  delay(500);
+  noTone(2);
+  tone(2,mi);
+  delay(500);
+  noTone(2);
+
+  tone(2,mi);
+  delay(800);
+  noTone(2);
+  tone(2,re);
+  delay(500);
+  noTone(2);
+  tone(2,re);
+  delay(500);
+  noTone(2);
+
+  delay(500);
+}
